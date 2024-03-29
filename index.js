9e48c0e68a6b037a2da753d9aaa4e126ea759bec
Abraham <abukip344@gmail.com>
2024-03-29 04:41:29 -0400

    content load

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function (){
+    const targetElement = document.getElementById('text')
+    targetElement.textContent = "This is really cool!";
+});
+
